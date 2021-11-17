3e5ea3002546cf87d53d55deca57a36c0b115295
박용수 <dydtn4748@naver.com>
2021-11-17 19:50:03 +0900

    2021-11-17 자바수업 깃허브 사용

---
+++ JavaStudy_naye/src/a01_첫수업/Kimnaye.java
@@ -0,0 +1,11 @@
+package a01_첫수업;
+
+public class Kimnaye {
+
+	public static void main(String[] args) {
+		System.out.println("김나예");
+		int i_number = 10;
+		i_number = 100;
+	}
+
+}
---
+++ JavaStudy_naye/src/a04_형변환/ConversionString.java
@@ -0,0 +1,27 @@
+package a04_형변환;
+
+public class ConversionString {
+
+	/**
+	 * 
+	 * String(문자열)
+	 * 
+	 */
+	public static void main(String[] args) {
+		char name1 = '김';
+		char name2 = '나';
+		char name3 = '예';
+		
+		System.out.print(name1);
+		System.out.print(name2);
+		System.out.print(name3);
+		
+		String name = "김나예";
+
+		System.out.println("" + name1 + name2 + name3 + "입니다.");
+		System.out.println(name + "입니다.");
+		System.out.println(10+"10");
+		
+	}
+	
+}
